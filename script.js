47df07b6102dd6bcbe554836c463bcc35830764b
Ajeet yadav <ajeetyadavajju88@gmail.com>
2026-01-30 17:36:16 +0530

    feat: add product click navigation and category data handling

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const products = document.querySelectorAll(".product-card");
     const itemsNumber = document.querySelector(".items-number");
 
-    
     itemsNumber.innerText = products.length + " items";
 
-    
-    const plusBtns = document.querySelectorAll(".button");
-
-    plusBtns.forEach((btn, index) => {
-        btn.addEventListener("click", () => {
-            console.log(`Product ${index + 1} added`);
+    products.forEach(product => {
+        product.addEventListener("click", () => {
+            const category = product.dataset.category;
+            window.location.href = `product.html?category=${category}`;
         });
     });
 });
