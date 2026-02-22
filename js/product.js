83f8882ac7f912bd7ae3a3919797497d258f0de4
Ajeet yadav <ajeetyadavajju88@gmail.com>
2026-02-22 20:06:24 +0530

    feat: dynamically update page title based on category

--- js/product.js
+++ js/product.js
@@ -1,3 +1,4 @@
 const params = new URLSearchParams(window.location.search);
 const category = params.get("category");
 const pageTitle = document.getElementById("page-title");
+pageTitle.textContent = category ? category.toUpperCase() : "Products";
